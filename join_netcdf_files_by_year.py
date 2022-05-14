b4b878ac04501d746cd4e56c3e77308074251451
Julio Isaac Montenegro Gambini <jmontenegrog@uni.edu.pe>
2022-05-14 22:34:20 +0200

    Update join_netcdf_files_by_year.py

--- join_netcdf_files_by_year.py
+++ join_netcdf_files_by_year.py
@@ -15,13 +15,14 @@
 # Please, when using this script, cite as: "Montenegro, J. (2022). Tools for
 # handling NASA NEX GDDP CMIP6 climate change datasets"
 
-
+#Loading the required packages
 import os
 import glob
 import netCDF4
 import xarray as xr
 
+#Using xarray library to join the files in a loop
 ds = xr.merge([xr.open_dataset(f) for f in glob.glob(r'D:/RESEARCH/CMIP6/R SCRIPTS CMIP6/NNGDDP_CMIP6_R/*.nc')])
 ds.to_netcdf(r'D:/RESEARCH/CMIP6/R SCRIPTS CMIP6/NNGDDP_CMIP6_R/joined_nc.nc')
-
+#Let`s open to verify the created netCDF file
 ds1 = xr.open_dataset(r'D:/RESEARCH/CMIP6/R SCRIPTS CMIP6/NNGDDP_CMIP6_R/joined_nc.nc')
